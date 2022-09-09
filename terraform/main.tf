7fc490dd5f1615d5413da8d7180b8d799c2105fe
blastomussa <jccourtn@gmail.com>
2022-09-08 22:04:31 -0400

    Terraform: ACR + ACR task to build image

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,12 +5,14 @@ DONE(ish):
   Resource Group
   CosmosDB Account
     Database + Collection
+  Azure Container Registry
+    Task to rebuild new backend docker image with every commit to master branch
+    Run task to create an initial Docker image for the backend API
 
 TO DO:
   vNet
   Azure Container Instance (w/ mongo ENV variables)
   App Service
-  Azure Container Registry?
   Application Gateway
   Any other networking components to route traffic correctly between tiers
     (Private Link, DNS Zone)
@@ -38,6 +40,42 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.resource_group_location
+}
+
+
+# Create Azure Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = var.container_registry_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
+
+# Build Docker image for backend API with ACR task
+# THIS NEEDS TO BE TESTED
+resource "azurerm_container_registry_task" "task" {
+  name                  = "build-task"
+  container_registry_id = azurerm_container_registry.acr.id
+  platform {
+    os = "Linux"
+  }
+  docker_step {
+    dockerfile_path      = "/backend/Dockerfile" //this might need to be changed
+    context_path         = "https://github.com/blastomussa/AzureProjectF22.git#master"
+    context_access_token = var.github_pat
+    image_names          = ["BackendAPI:latest"]
+  }
+  depends_on = [azurerm_container_registry.acr]
+}
+
+
+# Run the previously created task
+# THIS NEEDS TO BE TESTED
+resource "azurerm_container_registry_task_schedule_run_now" "build" {
+  container_registry_task_id = azurerm_container_registry_task.task.id
+  depends_on = [azurerm_container_registry_task.task]
 }
 
 
@@ -71,6 +109,7 @@ resource "azurerm_cosmosdb_mongo_database" "mongodb" {
   resource_group_name = azurerm_cosmosdb_account.acc.resource_group_name
   account_name        = azurerm_cosmosdb_account.acc.name
   throughput          = 400
+  depends_on = [azurerm_cosmosdb_mongo_database.acc]
 }
 
 
