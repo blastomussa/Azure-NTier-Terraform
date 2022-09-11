ed67ecd638733fb2fc96d391c4556f8fe2863b6d
blastomussa <jccourtn@gmail.com>
2022-09-10 23:32:21 -0400

    bug fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -344,9 +344,9 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 # Kubernetes provider credentials
 provider "kubernetes" {
   host = azurerm_kubernetes_cluster.cluster.kube_config.0.host
-  client_certificate     = azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate
-  client_key             = azurerm_kubernetes_cluster.cluster.kube_config.0.client_key
-  cluster_ca_certificate = azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
 }
 
 
