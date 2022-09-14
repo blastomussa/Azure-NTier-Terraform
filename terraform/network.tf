f9aede5160b01f4d51c3c11d217c23c64f25fbe4
blastomussa <jccourtn@gmail.com>
2022-09-14 09:07:30 -0400

    Terraform: NSG

--- terraform/network.tf
+++ terraform/network.tf
@@ -53,3 +53,37 @@ resource "azurerm_network_profile" "frontendprofile" {
   }
   depends_on = [azurerm_subnet.frontend]
 }
+
+resource "azurerm_network_security_group" "frontendnsg" {
+  name                = "frontend-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "allow-http"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "80"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "deny-all"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "frontendnsg" {
+  subnet_id                 = azurerm_subnet.frontend.id
+  network_security_group_id = azurerm_network_security_group.frontendnsg.id
+}
