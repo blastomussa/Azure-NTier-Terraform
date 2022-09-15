8c8851658ac0fbc57af1e44b5376ba4b8aef1e32
blastomussa <jccourtn@gmail.com>
2022-09-14 21:15:14 -0400

    Terraform: NSG + log analytics

--- terraform/aks-cluster.tf
+++ terraform/aks-cluster.tf
@@ -1,8 +1,36 @@
 # random generation of aks cluster name
 resource "random_pet" "prefix" {}
 
+# for log analytics unique name
+resource "random_id" "log_analytics_workspace_name_suffix" {
+  byte_length = 8
+}
+
+# log analytics
+resource "azurerm_log_analytics_workspace" "log" {
+  location            = var.resource_group_location
+  # The WorkSpace name has to be unique across the whole of azure;
+  # not just the current subscription/tenant.
+  name                = "${var.log_analytics_workspace_name}-${random_id.log_analytics_workspace_name_suffix.dec}"
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = var.log_analytics_workspace_sku
+}
+
+resource "azurerm_log_analytics_solution" "logsolution" {
+  location              = azurerm_log_analytics_workspace.log .location
+  resource_group_name   = azurerm_resource_group.rg.name
+  solution_name         = "ContainerInsights"
+  workspace_name        = azurerm_log_analytics_workspace.log.name
+  workspace_resource_id = azurerm_log_analytics_workspace.log.id
+
+  plan {
+    product   = "OMSGallery/ContainerInsights"
+    publisher = "Microsoft"
+  }
+}
+
 # Azure Kubernetes cluster
-resource "azurerm_kubernetes_cluster" "cluster" {
+resource "azurerm_kubernetes_cluster" "k8s" {
   name                = "${random_pet.prefix.id}-aks"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -10,8 +38,16 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 
   default_node_pool {
     name       = "default"
-    node_count = 1
+    node_count = var.node_count
     vm_size    = "Standard_D2_v2"
+  }
+
+  linux_profile {
+    admin_username = "ubuntu"
+
+    ssh_key {
+      key_data = file(var.ssh_public_key)
+    }
   }
 
   service_principal {
@@ -20,7 +56,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
   }
 
   network_profile {
-    network_plugin    = "azure"
+    network_plugin    = "kubenet"
     load_balancer_sku = "standard"
 
   }
